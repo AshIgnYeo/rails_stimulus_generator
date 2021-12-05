a386bf12f48f6b17ba3eaea5c88d77c186af79f3
AshIgnYeo <contact@ashleyignatius.com>
2021-12-05 23:53:12 +0800

    updated contents of template

--- stimulus/templates/controller.js
+++ stimulus/templates/controller.js
@@ -1,17 +1,26 @@
+/**
+ * Stimulus controller
+ * Documentation: https://stimulus.hotwired.dev/
+ * read more about lifecyles at https://stimulus.hotwired.dev/reference/lifecycle-callbacks
+ */
+
 import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = [];
 
   initialize() {
-    // Called once, when the controller is first instantiated
+    // Once, when the controller is first instantiated
+    console.log("Generated Controller Initialized!");
   }
 
   connect() {
-    // Called any time the controller is connected to the DOM
+    // Anytime the controller is connected to the DOM
+    console.log("Generated Controller Connected!");
   }
 
   disconnect() {
-    // Called any time the controller is disconnected from the DOM
+    // Anytime the controller is disconnected from the DOM
+    console.log("Generated Controller Disconnected!");
   }
 }
